fc6319e6591dc5a8af51f20e8871ce53f19abc5c
MrFierro <mrfierro@ya.ru>
2025-09-05 02:16:58 +0300

    Update install-full-bot.sh

--- scripts/bot/install-full-bot.sh
+++ scripts/bot/install-full-bot.sh
@@ -15,7 +15,7 @@ check_component() {
 
     case $component in
         "vsftpd")
-            if command -v vsftpd &> /dev/null; then
+            if command -v vsftpd &> /dev/null; && [ -f "/etc/vsftpd.conf" ] then
     		echo "1"
 	    else
     		echo "2"
