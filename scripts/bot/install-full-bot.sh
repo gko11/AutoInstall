2cb1714ebeec38b36fff39d4d450c3bc85f92453
MrFierro <mrfierro@ya.ru>
2025-09-05 02:08:35 +0300

    Update install-full-bot.sh

--- scripts/bot/install-full-bot.sh
+++ scripts/bot/install-full-bot.sh
@@ -11,11 +11,11 @@ REINSTALL_CADDY=false
 check_component() {
     local component=$1
     local path=$2
-    local env_file=$3
+    local file=$3
 
     case $component in
-        "panel")
-             if ! command -v vsftpd &> /dev/null; then
+        "vsftpd")
+             if command -v vsftpd &> /dev/null; || [ -f "$file" ] then
     		echo "1"
 	    else
     		echo "2"
@@ -282,7 +282,7 @@ show_panel_info() {
 }
 
 main() {
-    check_component "panel" "/opt/remnawave" "/opt/remnawave/.env"
+    check_component "panel" "/opt/remnawave" "/etc/vsftpd.conf"
     check_component "subscription" "/opt/remnawave/subscription" "/opt/remnawave/subscription/.env"
     check_component "caddy" "/opt/remnawave/caddy" "/opt/remnawave/caddy/.env"
 
