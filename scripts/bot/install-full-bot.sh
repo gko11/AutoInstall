336cc6e2e7459a15a8757de254b1c7508c1432bd
MrFierro <mrfierro@ya.ru>
2025-09-08 00:27:37 +0300

    Update install-full-bot.sh

--- scripts/bot/install-full-bot.sh
+++ scripts/bot/install-full-bot.sh
@@ -208,15 +208,18 @@ install_bot() {
   		sed -i "s|my.domen.com|$BOT_DOMAIN|g" /etc/caddy/Caddyfile
 	fi
 	sudo systemctl restart caddy
-	sudo apt update -y && sudo apt install postgresql postgresql-contrib -y
-	sudo systemctl start postgresql && sudo systemctl enable postgresql
-	su - postgres
-	createuser --interactive -n
-	createdb $DATABASE_TABLE_NAME --owner=$DATABASE_LOGIN_USERNAME
-	psql
-	ALTER USER $DATABASE_LOGIN_USERNAME WITH PASSWORD '$DATABASE_LOGIN_PASSWORD';
-	\q
-	exit
+	sudo apt update -y
+	sudo apt install -y postgresql postgresql-contrib
+
+# Запуск и автозапуск сервиса
+	sudo systemctl start postgresql
+	sudo systemctl enable postgresql
+
+# Создание пользователя и базы (НЕ через интерактив)
+	sudo -u postgres psql <<EOF
+	CREATE USER $DATABASE_LOGIN_USERNAME WITH PASSWORD '$DATABASE_LOGIN_PASSWORD';
+	CREATE DATABASE $DATABASE_TABLE_NAME OWNER $DATABASE_LOGIN_USERNAME;
+	EOF
 
 }
 
